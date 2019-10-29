a6e84c2785a23fc438b82ab69af2105c7e922338
Natalya <natalyamyunster@yahoo.com>
2019-10-29 18:56:00 +0100

    added cat and dog images

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,8 +5,8 @@ export default class Posts extends Component {
     render() {
         return(
             <div className="left">
-                <Post  src=".src/components/cat.png" alt=" cat siam"/>
-                <Post src=".src/components/dog.png" alt="jack russel"/>
+                <Post  src="src/cat.png" alt="cat siam" />
+                <Post src="src/dog.png" alt="jack russel" />
             </div>
         )
     }

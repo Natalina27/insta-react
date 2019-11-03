499a339657f78fe8dc5e6e1208bb05b47d2e7d28
Natalya <natalyamyunster@yahoo.com>
2019-11-03 15:07:37 +0100

    added constractor and check for Error

--- src/services/instaService.js
+++ src/services/instaService.js
@@ -1,10 +1,17 @@
 export default  class instaService {
     constructor(){
-
+        this._apiBase = 'http://localhost:3000';
     }
 
-    getResource = (url) => {
-        const res = fetch(url);
+    getResource = async (url) => {
+        const res = await fetch(`${this._apiBase}${url}`);
+        //fetch is API for server request
+
+        if(!res.ok){
+            throw new Error(`Could not fetch ${url}; recieved ${res.status}`);
+        }
+
+        return await res.json(); // transform json format to object
 
     }
 }

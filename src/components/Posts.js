0171b8162c08c944bf9227d58dfd8e96b1721d82
Natalya <natalyamyunster@yahoo.com>
2019-11-04 21:32:57 +0100

    added  instaService in Posts.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,15 +1,18 @@
 import React, {Component} from 'react';
+import InstaService from '../services/instaService';
 //import Post from "./Post";
-import cat from "../cat.png";
-import dog from "../dog.png";
+// import cat from "../cat.png";
+// import dog from "../dog.png";
 
 
 export default class Posts extends Component {
+    InstaService = new InstaService();
+
     render() {
         return(
             <div className="left">
-                <Post  src={cat} alt="cat siam" />
-                <Post src ={dog} alt="jack russel" />
+                {/*<Post  src={cat} alt="cat siam" />*/}
+                {/*<Post src ={dog} alt="jack russel" />*/}
             </div>
         )
     }

84753d67d4d267b19e2aebd6af41cb76a921bfe9
Natalya <natalyamyunster@yahoo.com>
2019-11-04 22:39:44 +0100

    added state : posts and error

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -7,7 +7,10 @@ import InstaService from '../services/instaService';
 
 export default class Posts extends Component {
     InstaService = new InstaService();
-
+    state = {
+        posts: [],
+        error: false
+    }
     render() {
         return(
             <div className="left">
